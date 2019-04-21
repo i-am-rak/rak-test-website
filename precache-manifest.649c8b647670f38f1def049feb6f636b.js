5bd1c9070d293185eb41e1c3d3a95fe18080ba52
Anuraag Shankar <raknahsa@gmail.com>
2019-04-20 20:10:47 -0400

    Updates

---
+++ precache-manifest.649c8b647670f38f1def049feb6f636b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52680d03842e2c308041",
+    "url": "/static/css/main.35534d2e.chunk.css"
+  },
+  {
+    "revision": "52680d03842e2c308041",
+    "url": "/static/js/main.45afe906.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a4d732d422a4e7750bc1b06b6df29073",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rak-test-website/precache-manifest.20ed3aed86a949ea04e8df265507bf55.js"
+  "/precache-manifest.649c8b647670f38f1def049feb6f636b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rak-test-website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
